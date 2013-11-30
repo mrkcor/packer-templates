31e6139c28f390ec7ed3db8724a3aac56e71a6a6
Mark Kremer <mark@5ec.nl>
2013-11-30 22:40:59 +0100

    Always update yum packages at the start for CentOS

--- scripts/vagrant-centos.sh
+++ scripts/vagrant-centos.sh
@@ -3,6 +3,9 @@
 # Exit when any of these commands fail
 set -e
 
+# Update all packages
+yum -y update
+
 # Install necessary libraries for guest additions and Vagrant NFS Share
 yum -y install gcc make automake autoconf libtool gcc-c++ kernel-headers-`uname -r` kernel-devel-`uname -r`
 
