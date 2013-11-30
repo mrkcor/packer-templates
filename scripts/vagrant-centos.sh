5675a989a34029bfb43692d4356aa548c88f1d10
Mark Kremer <mark@5ec.nl>
2013-11-30 23:33:44 +0100

    Adds dkms via EPEL

--- scripts/vagrant-centos.sh
+++ scripts/vagrant-centos.sh
@@ -3,11 +3,14 @@
 # Exit when any of these commands fail
 set -e
 
-# Update all packages
-yum -y update
-
-# Install necessary libraries for guest additions and Vagrant NFS Share
-yum -y install gcc make automake autoconf libtool gcc-c++ kernel-headers-`uname -r` kernel-devel-`uname -r`
-
 # Install wget (this is used in other provision scripts)
 yum -y install wget
+
+# Add EPEL repository
+rpm -ivh http://dl.fedoraproject.org/pub/epel/6/x86_64/epel-release-6-8.noarch.rpm
+
+# Install necessary libraries for guest additions and Vagrant NFS Share
+yum -y install gcc make automake autoconf libtool gcc-c++ kernel-headers kernel-devel dkms bzip2
+
+# Update all packages
+yum -y update
